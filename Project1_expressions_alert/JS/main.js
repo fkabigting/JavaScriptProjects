a9ba631ebeb7dc61c895c92992acd49454fe619f
fkabigting <leonardfrancis.kabigting1@my-aolcc.com>
2023-09-05 21:09:04 -0600

    Basic JavaScript Projects Assignment 1

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,35 @@
+
+//window.alert("Hello");
+//document.write('yawa');
+//var A="This is a string";
+//document.write(A);
+//var message = "Noli Me Tangere!";
+//document.write(message);
+//document.write("John said to Mike \"Stay here\"");
+//document.write("Noli" + " Me" + " Tangere");
+
+//Concat
+//var A='Noli';
+//var B='Me';
+//var C='Tangere';
+//document.write(A+" "+ B +" " + C);
+
+//multiple var declartion
+//var Family="Kabigting", Dad="Francis", Mom="Marsha", Daughter1="Sky", Daughter2="Lexi";
+//document.write(Family);
+
+//Expression
+//document.write(3+3);
+
+window.alert("Hit Okay");
+
+var sent1="This is the beggining of text";
+var sent2="and this is the end of the text";
+var a=1, b=2;
+
+document.write(sent1 + " " + sent2);
+document.write(" Total is:\n" + a+b);
+//the above code used variables to hold text values and numbers
+//the + sign combines the 2 characters
+//added two number using variables
+
