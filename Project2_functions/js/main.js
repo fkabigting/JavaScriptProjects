74463dcb6bee3a3a8db5d14ea969b9d638a70e73
fkabigting <leonardfrancis.kabigting1@my-aolcc.com>
2023-09-07 22:44:45 -0600

    calling javascript by onclick funxn in HTML file

---
+++ Project2_functions/js/main.js
@@ -0,0 +1,10 @@
+
+function printMessage() {
+  document.getElementById('output').innerHTML = "Pinalitan ko ang This is a paragraph";
+}
+
+function concat(){
+  var sentence= "El Filibusterismo";
+  sentence += " at Noli Mi Tangere";
+  document.getElementById('con').innerHTML = sentence;
+}
